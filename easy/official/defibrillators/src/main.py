b6645dff4bfcfb14bed36b204885b17e97d4ee2e
Sergio Strazzacappa <strazzacappasergio@gmail.com>
2026-01-04 14:25:09 -0300

    Defibrillators - Python

--- easy/official/defibrillators/src/main.py
+++ easy/official/defibrillators/src/main.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python3
+
+
 def distance(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-user_lon = float(input().replace(',', '.'))
-user_lat = float(input().replace(',', '.'))
+user_lon = float(input().replace(",", "."))
+user_lat = float(input().replace(",", "."))
 n = int(input())
 
 min_distance = 10000
 target = ""
+
 for i in range(n):
-    _, name, _, _, lon, lat = input().split(';')
-    lon = float(lon.replace(',', '.'))
-    lat = float(lat.replace(',', '.'))
+    _, name, _, _, lon, lat = input().split(";")
+    lon = float(lon.replace(",", "."))
+    lat = float(lat.replace(",", "."))
 
     d = distance(user_lon, user_lat, lon, lat)
 
